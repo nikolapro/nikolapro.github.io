f14e6dd3c06d7c13d155fc4e7e77fdf6b029387f
nexdon <nikola.petrovicnidza.mol@gmail.com>
2016-01-03 17:44:42 +0100

    Added translations

---
+++ useful/translate/translate.js
@@ -0,0 +1,24 @@
+function lolcat() {
+	var args = $("#translate").val();
+	if (args == 0) {
+		$("#translated").html("U DOAN WANTS 2 TRANZLATE ANYTHIN???");
+	}
+	else {
+		$.get('http://allow-any-origin.appspot.com/http://speaklolcat.com/?from=' + encodeURI(args)).success(function (data) {
+			var translated = new DOMParser().parseFromString(data, 'text/html').querySelector("#to").textContent;
+            $("#translated").html(translated);
+        });
+	}
+}
+function pirate() {
+	var args = $("#translate").val();
+	if (args == 0) {
+		$("#translated").html("U DOAN WANTS 2 TRANZLATE ANYTHIN???");
+	}
+	else {
+		$.get('http://allow-any-origin.appspot.com/http://isithackday.com/arrpi.php?text=' + encodeURI(args)).success(function (data) {
+			var translated = new DOMParser().parseFromString(data, 'text/html').querySelector("body").textContent;
+            $("#translated").html(translated);
+        });
+	}
+}
