3d41c8120b1d88d8ed24916f4e3d072d7bfd6deb
eyeball edward <edwardyeung.private@gmail.com>
2016-01-04 21:45:10 +0800

    I doez teh magixz

--- useful/translate/translate.js
+++ useful/translate/translate.js
@@ -1,36 +1,34 @@
 function lolcat() {
-	var args = $("#translate").val();
-	if (args == 0) {
-		$("#translated").html("U DOAN WANTS 2 TRANZLATE ANYTHIN???");
-	}
-	else {
-		$.get('http://allow-any-origin.appspot.com/http://speaklolcat.com/?from=' + encodeURI(args)).success(function (data) {
-			var translated = new DOMParser().parseFromString(data, 'text/html').querySelector("#to").textContent;
+    var args = $("#translate").val();
+    if (args == 0) {
+        $("#translated").html("U DOAN WANTS 2 TRANZLATE ANYTHIN???");
+    }
+    else {
+        $.get('http://allow-any-origin.appspot.com/http://speaklolcat.com/?from=' + encodeURI(args)).success(function (data) {
+            var translated = new DOMParser().parseFromString(data, 'text/html').querySelector("#to").textContent;
             $("#translated").html(translated);
         });
-	}
+    }
 }
 function pirate() {
-	var args = $("#translate").val();
-	if (args == 0) {
-		$("#translated").html("Don't want to translate anything???");
-	}
-	else {
-		$.get('http://allow-any-origin.appspot.com/http://isithackday.com/arrpi.php?text=' + encodeURI(args)).success(function (data) {
-			var translated = new DOMParser().parseFromString(data, 'text/html').querySelector("body").textContent;
+    var args = $("#translate").val();
+    if (args == 0) {
+        $("#translated").html("Don't want to translate anything???");
+    }
+    else {
+        $.get('http://allow-any-origin.appspot.com/http://isithackday.com/arrpi.php?text=' + encodeURI(args)).success(function (data) {
+            var translated = new DOMParser().parseFromString(data, 'text/html').querySelector("body").textContent;
             $("#translated").html(translated);
         });
-	}
+    }
 }
 function cowsay() {
-	var args = $("#translate").val();
-	if (args == 0) {
-		$("#cow").html("Mooooooooo, please add text!");
-	}
-	else {
-		$.get('http://allow-any-origin.appspot.com/http://cowsay.morecode.org/say?message=' + encodeURI(args) + '&format=text').success(function (data) {
-			var translated = data;
-            $("#cow").html(translated + "<hr style='margin-top: -20px;'>");
+    if (!$('#translate').length) {
+        $("#cow").html("Mooooooooo, please add text!");
+    }
+    else {
+        $.get('http://allow-any-origin.appspot.com/http://cowsay.morecode.org/say?message=' + encodeURI($('#translate').val()) + '&format=text').success(function (data) {
+            $("#cow").html(data + "<hr style='margin-top: -20px;'>");
         });
-	}
+    }
 }
